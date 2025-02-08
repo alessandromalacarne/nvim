400fb4335e3d12eef16460c62dfe9fc603ea1eb1
alsoasnerd <alsoasnerd@gmail.com>
2025-02-08 17:39:54 -0300

    Merge `old-nvim` into that gigachad config

--- init.lua
+++ init.lua
@@ -86,12 +86,17 @@ P.S. You can delete this when you're done too. It's your config now! :)
 
 -- Set <space> as the leader key
 -- See `:help mapleader`
+
+-- CUSTOM --
+require 'custom/keymaps'
+-- CUSTOM --
+
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -102,7 +107,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -793,7 +798,7 @@ require('lazy').setup({
 
           -- If you prefer more traditional completion keymaps,
           -- you can uncomment the following lines
-          --['<CR>'] = cmp.mapping.confirm { select = true },
+          ['<CR>'] = cmp.mapping.confirm { select = true },
           --['<Tab>'] = cmp.mapping.select_next_item(),
           --['<S-Tab>'] = cmp.mapping.select_prev_item(),
 
@@ -942,7 +947,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
---
+++ lua/custom/keymaps.lua
@@ -0,0 +1,58 @@
+-- delete without yanking
+vim.keymap.set('x', '<leader>P', [["_dP]])
+
+--- behave like other capitals
+vim.keymap.set('n', 'Y', 'y$')
+
+--- keeping it centered
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+vim.keymap.set('n', 'J', 'mzJ`z')
+
+-- make file executable
+vim.keymap.set('n', '<C-x>', '<cmd>!chmod +x %<CR>', { silent = true })
+
+--- moving text
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+
+-- open tmux sessionizer (like a real chad)
+vim.keymap.set('n', '<C-f>', '<cmd>silent !tmux neww tms<CR>')
+
+-- Fugitive
+vim.keymap.set('n', '<leader>gg', ':Git<CR>')
+vim.keymap.set('n', '<leader>gp', ':Git push<CR>')
+vim.keymap.set('n', '<leader>gP', ':Git pull --rebase<CR>')
+vim.keymap.set('n', '<leader>gd', ':Git diff<CR>')
+vim.keymap.set('n', '<leader>gl', ':Git log<CR>')
+
+-- CodeCompanion
+vim.keymap.set('n', '<leader>at', ':CodeCompanionChat<CR>')
+vim.keymap.set('v', '<leader>am', ':CodeCompanionActions<CR>')
+vim.keymap.set('n', '<leader>ac', ':CodeCompanionCmd')
+
+-- Harpoon
+vim.keymap.set('n', '<C-h>', function()
+  require('harpoon'):list():select(1)
+end)
+vim.keymap.set('n', '<C-j>', function()
+  require('harpoon'):list():select(2)
+end)
+vim.keymap.set('n', '<C-k>', function()
+  require('harpoon'):list():select(3)
+end)
+vim.keymap.set('n', '<C-l>', function()
+  require('harpoon'):list():select(4)
+end)
+vim.keymap.set('n', '<C-p>', function()
+  require('harpoon'):list():prev()
+end)
+vim.keymap.set('n', '<C-n>', function()
+  require('harpoon'):list():next()
+end)
+vim.keymap.set('n', '<C-o>', function()
+  local harpoon = require 'harpoon'
+  harpoon.ui:toggle_quick_menu(harpoon:list())
+end)
